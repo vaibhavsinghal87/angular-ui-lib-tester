35105da5bd1bfc8373f3b8e681bfdcfe956caf8a
vaibhavsinghal910 <vaibhavsinghal87@gmail.com>
2019-02-08 03:19:54 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-ui-lib-tester';
+
+  navigate(evt) {
+
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { UiButtonComponent } from 'ui-button';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UiButtonComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
